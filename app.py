e218606830810a5d3b1390d140b0f6d1c37863ba
raghavag1997 <raghav.15jics089@jietjodhpur.ac.in>
2020-04-26 18:42:57 +0530

    Chnges made by servet again 1

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)             # create an app instance
 def hello():                      # call method hello
     return "Hello Servet thank for always supporting me"         # which returns "hello world"
 if __name__ == "__main__":        # on running python app.py
-    app.run(debug=True,host='0.0.0.0', 5000)                     # run the flask app
+    app.run(debug=True,host='0.0.0.0', port=5000)                     # run the flask app
