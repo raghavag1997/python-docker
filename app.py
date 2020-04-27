c791db3abb931b6f045b3103e9214b7440e1f325
raghavag1997 <raghav.15jics089@jietjodhpur.ac.in>
2020-04-27 11:51:22 +0530

    Chnges made by raghav

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)             # create an app instance
 
 @app.route("/")                   # at the end point /
 def hello():                      # call method hello
-    return "Hello Servet thank for always supporting me, Staf safe love from india"         # which returns "hello world"
+    return "Hello World"         # which returns "hello world"
 if __name__ == "__main__":        # on running python app.py
     app.run(debug=True,host='0.0.0.0', port=5000)                     # run the flask app
